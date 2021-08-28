8e07edc63c716662c8c222e36f33edf500343df5
Abhi Koradia <abhikoradia@gmail.com>
2021-08-28 12:01:44 +0530

    Updates

---
+++ precache-manifest.1301c5994eb8d70fbdaa58ec2eb291b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d22474bf0623db9ca9325a69629a8cca",
+    "url": "/Calculator/index.html"
+  },
+  {
+    "revision": "024455dab5815cea8ab2",
+    "url": "/Calculator/static/css/main.53c037f2.chunk.css"
+  },
+  {
+    "revision": "37de524be2d075705b7f",
+    "url": "/Calculator/static/js/2.a12aa491.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Calculator/static/js/2.a12aa491.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "024455dab5815cea8ab2",
+    "url": "/Calculator/static/js/main.946b336b.chunk.js"
+  },
+  {
+    "revision": "0a79fa1d2af7875576c8",
+    "url": "/Calculator/static/js/runtime-main.8483c221.js"
+  }
+]);
